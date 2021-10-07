b789b691acbc3abf793b42adc0f9d3ed0cf7b454
Abdul karim Yassin <karim.mmd12@gmail.com>
2021-10-07 09:47:13 +0200

    change class component to functional component

--- src/components/common/active.tsx
+++ src/components/common/active.tsx
@@ -1,21 +1,18 @@
-import React, { Component, MouseEventHandler } from 'react'
+import React, { FunctionComponent, MouseEventHandler } from 'react'
 
 interface ActiveProps {
   isActive: boolean
   onClick: MouseEventHandler<HTMLButtonElement>
 }
-interface ActiveState {}
-class Active extends Component<ActiveProps, ActiveState> {
-  render() {
-    let classes = 'btn btn-sm '
-    const { isActive } = this.props
-    classes += isActive ? 'btn-success' : 'btn-danger'
-    return (
-      <button className={classes} onClick={this.props.onClick}>
-        {isActive ? 'Active' : 'Pending'}
-      </button>
-    )
-  }
-}
 
+const Active: FunctionComponent<ActiveProps> = (props) => {
+  let classes = 'btn btn-sm '
+  const { isActive } = props
+  classes += isActive ? 'btn-success' : 'btn-danger'
+  return (
+    <button className={classes} onClick={props.onClick}>
+      {isActive ? 'Active' : 'Pending'}
+    </button>
+  )
+}
 export default Active
