8ecd71c33dd3acadeffac5197f38c1aa80c6d40f
Abdul karim Yassin <karim.mmd12@gmail.com>
2021-10-07 12:35:56 +0200

    sort table on different columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Persons from './components/persons';
+import React from 'react'
+import Persons from './components/persons'
 
 function App() {
   return (
-    <main className="container">
+    <main className='container'>
       <Persons />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/persons.tsx
+++ src/components/persons.tsx
@@ -2,9 +2,19 @@ import React, { Component } from 'react'
 import { Person } from '../interfaces/Person'
 import { getPersons } from '../services/fakePersonsService'
 import Active from './common/active'
+import _ from 'lodash'
 
 class Persons extends Component {
-  state = { persons: getPersons() }
+  state: {
+    persons: Person[]
+    sortBy: { path: string; order: 'asc' | 'desc' }
+  } = {
+    persons: [],
+    sortBy: { path: 'name', order: 'asc' },
+  }
+  componentDidMount = () => {
+    this.setState({ persons: getPersons() })
+  }
   handleClick = (person: Person) => {
     const persons = [...this.state.persons]
     const index = persons.indexOf(person)
@@ -12,21 +22,41 @@ class Persons extends Component {
     persons[index].isActive = !persons[index].isActive
     this.setState({ persons })
   }
+  handleSortClick(path: string): void {
+    const sortBy = { ...this.state.sortBy }
+    if (sortBy.path === path)
+      sortBy.order = sortBy.order === 'asc' ? 'desc' : 'asc'
+    else {
+      sortBy.path = path
+      sortBy.order = 'asc'
+    }
+    this.setState({ sortBy })
+  }
   render() {
-    const { length: count } = this.state.persons
+    const { persons, sortBy } = this.state
+    const { length: count } = persons
+    const sortedPersons = _.orderBy(persons, [sortBy.path], [sortBy.order])
     if (count === 0) return <p>There are no persons</p>
     return (
       <table className='table table-striped'>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Company</th>
-            <th>Status</th>
+            <th onClick={() => this.handleSortClick('name')}>
+              Name<i className='fa fa-sort' aria-hidden='true'></i>
+            </th>
+            <th onClick={() => this.handleSortClick('gender')}>
+              Gender<i className='fa fa-sort' aria-hidden='true'></i>
+            </th>
+            <th onClick={() => this.handleSortClick('company')}>
+              Company<i className='fa fa-sort' aria-hidden='true'></i>
+            </th>
+            <th onClick={() => this.handleSortClick('isActive')}>
+              Status<i className='fa fa-sort' aria-hidden='true'></i>
+            </th>
           </tr>
         </thead>
         <tbody>
-          {this.state.persons.map((person) => (
+          {sortedPersons.map((person) => (
             <tr key={person.id}>
               <td>{person.name}</td>
               <td>{person.gender}</td>
